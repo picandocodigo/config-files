d4a4aa5f3fa2caa8e59716cf0d906cef672f9236
Fernando Briano <fernando@picandocodigo.net>
2019-01-18 20:45:59 +0000

    Add desktop entries, update init and README

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Configuration files
 
-Emacs configuration:
+## Init file
+
+The `init.sh` file will:
+
+- Check if the dotfiles are present and ask you to overwrite or skip
+- Copy any files in `desktop-entries` to `~/.local/share/applications/`
+- Try to install some common packages (Debian based distros)
+
+## Emacs configuration:
  - [Emacs config](https://github.com/picandocodigo/emacs-config)
  - [Spacemacs config](https://github.com/picandocodigo/spacemacs-config)
--- init.sh
+++ init.sh
@@ -1,5 +1,16 @@
 # Set up dot files:
 echo "Setting up dotfiles"
 for file in ~/workspace/picandocodigo/config-files/dotfiles/*; do
-    cd ~ && ln -s $file .${file##*/}
+    # TODO: Check if they exist, ask to overwrite or skip
+    ln -s $file ~/.${file##*/}
 done
+
+# Copy desktop entries to the applications directory
+for file in ~/workspace/picandocodigo/config-files/desktop-entries/*; do
+    cp $file ~/.local/share/applications/
+done
+
+# packages to install:
+# TODO: Ask if you want to install them
+echo "Installing common packages:"
+sudo apt-get install git tmux emacs
