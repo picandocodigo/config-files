5725ecb0b427223d7df68626eddc1cc7cfd51c7e
Fernando Briano <fernando@picandocodigo.net>
2020-10-22 22:16:41 +0100

    Minor update

--- init.sh
+++ init.sh
@@ -49,7 +49,7 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     echo "Installing common packages:"
-    sudo apt-get install git tmux emacs curl gimp colordiff
+    sudo apt-get install git tmux emacs curl gimpc olordiff peek vlc
 fi
 
 # ASDF
@@ -84,3 +84,4 @@ fi
 # sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" >> /etc/apt/sources.list.d/pgdg.list
 # wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 # sudo apt-get update
+
