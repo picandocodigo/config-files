abe43a6894575a2f4af746ea85295afb1be33c45
Fernando Briano <fernando@picandocodigo.net>
2022-05-16 16:10:35 +0100

    Adds docker installation to init.sh

--- init.sh
+++ init.sh
@@ -64,6 +64,23 @@ then
     echo "Now install one of the emacs packages"
 fi
 
+echo
+read -p "Install docker repositories? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "adding Docker's official GPG key:"
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "Adding repo to sources"
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+fi
+
+
 # ASDF
 read -p "Do you want to install asdf-vm? " -n 1 -r
 echo
