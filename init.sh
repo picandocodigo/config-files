617bb35cab3fa3ea5e4342510a1920c61bbe32ca
Fernando Briano <fernando@picandocodigo.net>
2024-09-03 09:17:39 +0100

    Updates rvm installation

--- init.sh
+++ init.sh
@@ -99,7 +99,7 @@ read -p "Do you want to install rvm? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+    gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
     curl -sSL https://get.rvm.io | bash -s stable --ruby
 fi
 
